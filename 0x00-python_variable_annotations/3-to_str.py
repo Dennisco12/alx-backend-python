e127291d4a44b238a257bb083cae10df11906ad5
Dennisco12 <akinwonjowodennisco@gmail.com>
2023-02-02 12:25:15 -0800

    Added task 0 to 4

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""This contains a type annotated function"""
+
+
+def to_str(n: float) -> str:
+    return str(n)
---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""This contains an annotated function"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    sum = 0
+    for i in input_list:
+        sum += i
+    return sum
