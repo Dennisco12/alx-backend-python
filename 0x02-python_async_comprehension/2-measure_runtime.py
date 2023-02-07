d3c30f61559cbfbfac8a7f70bfc3faf8dc9d1559
Dennisco12 <akinwonjowodennisco@gmail.com>
2023-02-07 11:03:03 -0800

    Added task 0 to 2

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -9,6 +9,6 @@ async def measure_runtime() -> float:
     """This is the function definition"""
     start = time.perf_counter()
     await asyncio.gather(async_comprehension(), async_comprehension(),
-                    async_comprehension(), async_comprehension())
+                         async_comprehension(), async_comprehension())
     total_time = time.perf_counter() - start
     return total_time
